e8de9570bcca3343fa37c623fbcd9319c782261b
Chris Beer <cabeer@stanford.edu>
2018-07-25 16:46:22 -0700

    Add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :endpoints
   resources :searches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
