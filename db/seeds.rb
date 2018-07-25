75316dfc7aeb49069a920c2bddee1fd16e9ac027
Jack Reed <phillipjreed@gmail.com>
2018-07-25 16:58:39 -0600

    switch to sw-solr-g

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Endpoint.create(url: 'http://searchworks-solr-lb.stanford.edu:8983/solr/current/select')
+Endpoint.create(url: 'http://sw-solr-g.stanford.edu:8983/sw-relevancy/select')
 Endpoint.create(url: 'https://sul-solr-tester.stanford.edu/searchworks-dev/select')
 
 Search.create(query_params: 'defType=edismax&q=%7B%21pf2%3D%24p2+pf3%3D%24pf3%7D%2220th+-+21st+centuries%22&qt=search&sort=score+desc%2C+pub_date_sort+desc%2C+title_sort+asc')
