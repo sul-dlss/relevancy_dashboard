a38a14dd6ef079d5b33e98215804247affd42b01
Chris Beer <cabeer@stanford.edu>
2018-07-25 16:19:18 -0700

    Launch sidekiq workers on the app

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,5 +3,5 @@ server 'sul-solr-tester.stanford.edu', user: 'solr', roles: %w(web db app)
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
 
-set :sidekiq_roles, :background
+set :sidekiq_roles, :app
 set :sidekiq_processes, 10
