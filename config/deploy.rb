da16e621d7fca9d551fa9a844de11e666440ec51
Chris Beer <cabeer@stanford.edu>
2018-07-25 15:54:36 -0700

    Add secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w(config/database.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml)
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads config/settings)
