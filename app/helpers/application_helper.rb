1f5d1a08504e20b1a8e1f9496b37341359ad6ade
Chris Beer <cabeer@stanford.edu>
2018-11-26 11:32:32 -0800

    Update rubocop targetrubyversion to 2.5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def position_change_style(position_change)
     return '🚫' if position_change.nil?
     return if position_change.zero?
+
     if position_change.positive?
       content_tag 'span', "⬆︎#{position_change}", style: 'color: #998ec3'
     else
@@ -22,37 +23,36 @@ module ApplicationHelper
     navigator = lambda do |x|
       collapse_id = "collapse-#{i += 1}"
 
-      details = if x['description'] =~ Regexp.union(/max plus/, /sum of/)
-        Array(x['details']).sort_by { |y| y['value'] }.reverse
-      else
-        Array(x['details'])
-      end
+      details = if x['description']&.match?(Regexp.union(/max plus/, /sum of/))
+                  Array(x['details']).sort_by { |y| y['value'] }.reverse
+                else
+                  Array(x['details'])
+                end
 
-      default_collapse_state = if x['description'] =~ Regexp.union(/max plus/, /sum of/)
-        'show'
-      else
-        ''
-      end
+      default_collapse_state = if x['description']&.match?(Regexp.union(/max plus/, /sum of/))
+                                 'show'
+                               else
+                                 ''
+                               end
 
       explain_class = case x['description']
-      when /weight\(/
-        'weight'
-      when /max plus/
-        'max-plus'
-      when /sum of/
-        'sum'
-      when /product of/
-        'product'
-      else
-        ''
-      end
+                      when /weight\(/
+                        'weight'
+                      when /max plus/
+                        'max-plus'
+                      when /sum of/
+                        'sum'
+                      when /product of/
+                        'product'
+                      else
+                        ''
+                      end
 
       content_tag(:li,
-        content_tag(:span, x['value'], class: 'badge badge-default') +
-        content_tag(:span, x['description'].html_safe, data: { toggle: :collapse, target: "##{collapse_id}"}) +
-        content_tag(:ul, details.map { |y| navigator.call(y) }.join("\n").html_safe, id: collapse_id, class: "details collapse #{default_collapse_state}"),
-        class: explain_class
-      )
+                  content_tag(:span, x['value'], class: 'badge badge-default') +
+                  content_tag(:span, x['description'].html_safe, data: { toggle: :collapse, target: "##{collapse_id}" }) +
+                  content_tag(:ul, details.map { |y| navigator.call(y) }.join("\n").html_safe, id: collapse_id, class: "details collapse #{default_collapse_state}"),
+                  class: explain_class)
     end
 
     if explain.is_a? Hash
