7d7644d8ba516690dfc028b9e1b7ac2a9ddb57a8
Chris Beer <cabeer@stanford.edu>
2020-03-30 14:02:55 -0700

    Point cap at the new server

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
-server 'sul-solr-tester.stanford.edu', user: 'solr', roles: %w(web db app)
+server 'sw-relevancy-prod.stanford.edu', user: 'blacklight', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
