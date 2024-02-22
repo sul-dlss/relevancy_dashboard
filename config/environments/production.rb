cb9c4a95439ecc7d45560f047927aa7edfa37c6d
Steve Taylor <sctaylor@stanford.edu>
2024-02-22 08:24:04 -0800

    Use sidekiq for active jobs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "relevancy_dashboard_production"
 
+  # Use sidekiq for active jobs
+  config.active_job.queue_adapter = :sidekiq
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
