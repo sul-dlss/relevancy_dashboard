1a9cef26acbc8a9d11493bc187a4624b37634232
Chris Beer <cabeer@stanford.edu>
2026-05-28 10:34:26 -0700

    Update to Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_213203) do
+ActiveRecord::Schema[8.1].define(version: 2022_06_20_213203) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
     t.integer "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.integer "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.integer "byte_size", null: false
+    t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", precision: nil, null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -40,26 +40,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_213203) do
   end
 
   create_table "endpoints", force: :cascade do |t|
-    t.string "url"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.string "url"
   end
 
   create_table "search_results", force: :cascade do |t|
-    t.text "response", limit: 16777216
-    t.integer "endpoint_id"
-    t.integer "search_id"
     t.datetime "created_at", precision: nil, null: false
+    t.integer "endpoint_id"
+    t.text "response", limit: 16777216
+    t.integer "search_id"
     t.datetime "updated_at", precision: nil, null: false
     t.index ["endpoint_id"], name: "index_search_results_on_endpoint_id"
     t.index ["search_id"], name: "index_search_results_on_search_id"
   end
 
   create_table "searches", force: :cascade do |t|
-    t.text "query_params"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.text "query_params"
     t.decimal "score"
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
